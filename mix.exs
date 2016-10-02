d7f12576ba1854f52314a55dcc3ef5b44052e34c
Brian Glusman <brian@stellaservice.com>
2016-10-02 11:17:51 -0400

    Add dialyzer, credo and exdoc

--- mix.exs
+++ mix.exs
@@ -31,7 +31,10 @@ defmodule Exirc.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      {:scribe, "~>0.1.0"}
+      {:scribe, "~>0.1.0"},
+      {:ex_doc, "~> 0.13", only: :dev},
+      {:dialyxir, "~> 0.3", only: :dev},
+      {:credo, "~> 0.4", only: [:dev, :test]}
     ]
   end
 end
