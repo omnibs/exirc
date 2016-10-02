d974d097433b8039cb7dd3aec7fcaa26c43a8676
Brian Glusman <brian@stellaservice.com>
2016-10-01 23:02:48 -0400

    Working via if else

--- lib/nick_registry.ex
+++ lib/nick_registry.ex
@@ -5,25 +5,36 @@ defmodule NickRegistry do
     Agent.start_link(fn -> %NickRegistry{} end, name: __MODULE__)
   end
 
-  def nick(nick) do
+  def pid(nick) do
     Agent.get(__MODULE__, fn registry -> registry.nick_map[nick] end)
   end
 
+  def port(nick) do
+    Agent.get(__MODULE__, fn registry -> registry.port_map[registry.nick_map[nick]] end)
+  end
+
+
   def change_nick(old_nick, new_nick) do
-    Agent.get_and_update(__MODULE__, fn
-      registry = %NickRegistry{nick_map: %{^old_nick => pid} } ->
+    Agent.get_and_update(__MODULE__, fn registry ->
+      if Map.has_key?(registry.nick_map, old_nick) do
+        %{^old_nick => pid} = registry.nick_map
         {:ok, %{ registry | nick_map: %{registry.nick_map | new_nick => pid } } }
-      registry ->
+      else
         {:error, registry}
+      end
     end)
   end
 
   def set_nick(nick, pid, port) do
-    Agent.get_and_update(__MODULE__, fn
-      registry = %NickRegistry{nick_map: %{^nick => pid} } ->
+    Agent.get_and_update(__MODULE__, fn registry ->
+      if Map.has_key?(registry.nick_map, nick) do
         {:error, registry}
-      registry ->
-        {:ok, %NickRegistry{ nick_map: %{registry.nick_map | nick => pid }, port_map: %{registry.port_map | port => pid } } }
+      else
+        {:ok, %NickRegistry{ nick_map: Map.put(registry.nick_map, nick, pid),
+                             port_map: Map.put(registry.port_map, pid, port)
+                           }
+        }
+      end
     end)
   end
 
