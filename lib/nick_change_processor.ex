ef7c0b92175d2ea8ad8c8458d99b9b4d1a49bb24
Brian Glusman <brian@stellaservice.com>
2016-10-02 21:01:58 -0400

    Extract nick commands to change processor

---
+++ lib/nick_change_processor.ex
@@ -0,0 +1,27 @@
+defmodule NickChangeProcessor do
+
+  def change_nick(old_nick, new_nick) do
+    Agent.get_and_update(NickRegistry, fn registry ->
+      if Map.has_key?(registry.nick_map, old_nick) do
+        %{^old_nick => pid} = registry.nick_map
+        {:ok, %{ registry | nick_map: %{registry.nick_map | new_nick => pid } } }
+      else
+        {:error, registry}
+      end
+    end)
+  end
+
+  def set_nick(nick, pid, port) do
+    Agent.get_and_update(NickRegistry, fn registry ->
+      if Map.has_key?(registry.nick_map, nick) do
+        {:error, registry}
+      else
+        {:ok, %NickRegistry{ nick_map: Map.put(registry.nick_map, nick, pid),
+                             port_map: Map.put(registry.port_map, pid, port)
+                           }
+        }
+      end
+    end)
+  end
+
+end
--- lib/nick_registry.ex
+++ lib/nick_registry.ex
@@ -13,31 +13,6 @@ defmodule NickRegistry do
     Agent.get(__MODULE__, fn registry -> registry.port_map[registry.nick_map[nick]] end)
   end
 
-
-  def change_nick(old_nick, new_nick) do
-    Agent.get_and_update(__MODULE__, fn registry ->
-      if Map.has_key?(registry.nick_map, old_nick) do
-        %{^old_nick => pid} = registry.nick_map
-        {:ok, %{ registry | nick_map: %{registry.nick_map | new_nick => pid } } }
-      else
-        {:error, registry}
-      end
-    end)
-  end
-
-  def set_nick(nick, pid, port) do
-    Agent.get_and_update(__MODULE__, fn registry ->
-      if Map.has_key?(registry.nick_map, nick) do
-        {:error, registry}
-      else
-        {:ok, %NickRegistry{ nick_map: Map.put(registry.nick_map, nick, pid),
-                             port_map: Map.put(registry.port_map, pid, port)
-                           }
-        }
-      end
-    end)
-  end
-
   def destroy do
     Agent.stop(__MODULE__)
   end
