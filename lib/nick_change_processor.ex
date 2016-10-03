138e694fee9103da57916318c38fc5c51905eb91
Brian Glusman <brian@stellaservice.com>
2016-10-02 22:15:27 -0400

    Renamed to UserRegistry and switch port map

--- lib/nick_change_processor.ex
+++ lib/nick_change_processor.ex
@@ -1,7 +1,7 @@
 defmodule NickChangeProcessor do
 
   def change_nick(old_nick, new_nick) do
-    Agent.get_and_update(NickRegistry, fn registry ->
+    Agent.get_and_update(UserRegistry, fn registry ->
       if Map.has_key?(registry.nick_map, old_nick) do
         %{^old_nick => pid} = registry.nick_map
 
@@ -13,13 +13,13 @@ defmodule NickChangeProcessor do
     end)
   end
 
-  def set_nick(nick, pid, port) do
-    Agent.get_and_update(NickRegistry, fn registry ->
+  def set_nick(pid, port, nick \\ nil) do
+    Agent.get_and_update(UserRegistry, fn registry ->
       if Map.has_key?(registry.nick_map, nick) do
         {:error, registry}
       else
-        {:ok, %NickRegistry{ nick_map: Map.put(registry.nick_map, nick, pid),
-                             port_map: Map.put(registry.port_map, pid, port)
+        {:ok, %UserRegistry{ nick_map: Map.put(registry.nick_map, nick, pid),
+                             port_map: Map.put(registry.port_map, port, pid)
                            }
         }
       end
