8c9f3579e31dbfe46c158d9e12f31aec0bb6a280
Brian Glusman <brian@stellaservice.com>
2016-10-10 16:04:53 -0400

    Allow join/leave multiple rooms with comma-list

--- lib/room_processor.ex
+++ lib/room_processor.ex
@@ -1,11 +1,13 @@
 defmodule RoomProcessor do
-  def join(room, user_pid) do
-    RoomRegistry.pid_from_channel(room)
-    |> Room.add_user(user_pid)
+  def join(room_or_rooms, user_pid) do
+    each_room(room_or_rooms, fn room ->
+      RoomRegistry.pid_from_channel(room)
+      |> Room.add_user(user_pid)
+    end)
     # TODO send to user who joined
     # JOIN reply:
     # :jasnira!~Juliano@207.251.103.46 JOIN #channel * :realname
-    
+
     # RPL_NAMREPLY:
     # :serverhost 353 jasnira @ #channel :@jasnira
 
@@ -16,11 +18,17 @@ defmodule RoomProcessor do
     # idk we gotta tcpdump/wireshark and see
   end
 
-  def part(room, user_pid) do
-    RoomRegistry.pid_from_channel(room)
-    |> Room.remove_user(user_pid)
+  def part(room_or_rooms, user_pid) do
+    each_room(room_or_rooms, fn room ->
+      RoomRegistry.pid_from_channel(room)
+      |> Room.remove_user(user_pid)
+    end)
     # TODO send reply to user
     # TODO notify people on the channel
   end
 
+  defp each_room(rooms, room_function) do
+    String.split(rooms, ",")
+    |> Enum.each(room_function)
+  end
 end
